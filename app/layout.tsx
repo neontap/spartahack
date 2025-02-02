b70c420ba8a1eb5a08f168711eceb6747a269b1f
Gabe McGuire <gabepmcguire@gmail.com>
2025-02-01 19:31:49 -0500

    feat: enhance UI components and improve navigation structure
    - Update navigation bar by removing unnecessary border
    - Simplify university fetching by removing pagination parameters
    - Introduce a new search input with an attached popover for search results
    - Replace certain imports with grouped imports for better organization
    - Add Dialog and Popover components for improved user interaction
    - Update dependencies in package.json to include new UI components
    
    Signed-off-by: Gabe McGuire <gabepmcguire@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,11 +40,12 @@ export default function RootLayout({
         >
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full z-50 bg-md-purple flex justify-center border-b border-b-foreground/10 h-16">
+              <nav className="w-full z-50 bg-md-purple flex justify-center  h-16">
                 <div className="w-full max-w-full flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Image 
                       src="coursecheckminilogo.svg" 
+                      className="mt-2"
                       width={66} 
                       height={52} 
                       alt="Course Checker logo"
