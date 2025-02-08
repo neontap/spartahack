88da65aa7dee7bf378618267a3c5377ba7867fd1
Dmitry <starodu5@gmail.com>
2025-02-08 17:30:48 -0500

    logo loading fix, lazyload enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,12 +52,11 @@ export default function RootLayout({
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href="/">
                       <Image
-                          src="coursecheckminilogo.svg"
+                          src="/coursecheckminilogo.svg"
                           className=""
                           width={66}
                           height={52}
                           alt="Course Checker logo"
-                          priority={true}
                       />
                     </Link>
                   </div>
